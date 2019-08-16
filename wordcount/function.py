848f466dd14be76da4d6fc166c9c5e2034620df9
Unknown <781637982@qq.com>
2019-08-16 16:03:05 +0800

    添加了文字频率的统计
    添加了用户输入文字的出现频率统计，并按照出现的频率进行排序

--- wordcount/function.py
+++ wordcount/function.py
@@ -17,12 +17,14 @@ def count(request):
         else:
             word_dict[word]+=1
 
-
+        #字典排序
+        sorted_dict=sorted(word_dict.items(),key=lambda w:w[1],reverse=True)
 
     return render(request,'count.html',
                   {'count':total_count,
                    'text':user_text,
-                   'worddict':word_dict})
+                   'worddict':word_dict,
+                  'sorteddict':sorted_dict})
 
 
 def about(request):
